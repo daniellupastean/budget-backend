7535d256f06a3cd0080259042e5cb154dfeab519
Daniel Lupastean <lupasteandaniell1@gmail.com>
2024-01-25 19:03:10 +0200

    Added logging to banks service

--- src/app/banks/banks.service.ts
+++ src/app/banks/banks.service.ts
@@ -1,23 +1,50 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Bank } from './bank.entity';
 import { Repository } from 'typeorm';
+import { Logger } from 'winston';
 
 @Injectable()
 export class BanksService {
   constructor(
     @InjectRepository(Bank)
     private banksRepository: Repository<Bank>,
+    @Inject('WINSTON_LOGGER')
+    private logger: Logger,
   ) {}
 
   async getAllBanks() {
-    return await this.banksRepository.find({
-      select: { id: true, name: true },
-      where: { isDeleted: false },
-    });
+    this.logger.info('Fetching all banks');
+    try {
+      const banks = await this.banksRepository.find({
+        select: { id: true, name: true },
+        where: { isDeleted: false },
+      });
+      this.logger.info(`Found ${banks.length} banks`);
+      return banks;
+    } catch (error) {
+      this.logger.error(`Error fetching all banks: ${error.message}`);
+      throw error;
+    }
   }
 
   async softDeleteBank(id: string) {
-    return await this.banksRepository.update(id, { isDeleted: true });
+    this.logger.info(`Soft deleting bank with ID: ${id}`);
+    try {
+      const updateResult = await this.banksRepository.update(id, {
+        isDeleted: true,
+      });
+      if (updateResult.affected > 0) {
+        this.logger.info(`Bank soft deleted with ID: ${id}`);
+      } else {
+        this.logger.warn(`No bank found with ID: ${id} to soft delete`);
+      }
+      return updateResult;
+    } catch (error) {
+      this.logger.error(
+        `Error soft deleting bank with ID ${id}: ${error.message}`,
+      );
+      throw error;
+    }
   }
 }
